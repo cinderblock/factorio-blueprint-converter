8d99ed97c0f20b9e5b38d265ab73b78f833a1aab
Cameron Tacklind <cameron@tacklind.com>
2025-01-24 23:16:04 -0800

    Saner timeouts. Always finish. Better annotations

--- test/samples.test.ts
+++ test/samples.test.ts
@@ -1,15 +1,16 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, afterAll } from 'vitest';
 import { parseBlueprintData } from '../src/index.js';
 import { createReadStream } from 'node:fs';
 import { parse } from 'yaml';
 import { checkBlueprintDataMatchesString } from './helpers/compare.js';
-import { mkdir, readdir, readFile } from 'node:fs/promises';
+import { mkdir, readdir, readFile, stat } from 'node:fs/promises';
 import { join } from 'node:path';
 import annotationWriter from './helpers/annotationWriter.js';
+import { BlueprintData } from '../src/BlueprintData.js';
 
 const samplesDir = 'test/samples';
 
-describe('Blueprint Parser', { concurrent: true }, () => {
+describe('Blueprint Parser', { concurrent: true, timeout: 1000 }, () => {
   describe('Samples', async () => {
     // We have the blueprint strings in a yaml file for some samples
     const blueprintStrings = parse(await readFile(join(samplesDir, 'exports.yaml'), 'utf-8')) as Record<string, string>;
@@ -21,29 +22,31 @@ describe('Blueprint Parser', { concurrent: true }, () => {
     const sampleFiles = (await readdir(samplesDir)).filter(file => file.endsWith('.dat'));
 
     for (const sample of sampleFiles) {
-      describe(sample, () => {
-        it('should parse', { timeout: 20000 }, async () => {
-          const path = join(samplesDir, sample);
+      describe(sample, async () => {
+        const path = join(samplesDir, sample);
 
-          // Capture the data and any potential error
-          let data;
-          const stream = createReadStream(path);
-          const annotation = await annotationWriter(join(annotationsDir, `${sample}.txt`));
-          try {
-            data = await parseBlueprintData(stream, annotation);
-          } finally {
-            await annotation.finish(stream);
-          }
+        // Capture the data and any potential error
+        let data: BlueprintData;
+        const stream = createReadStream(path);
+        const annotation = annotationWriter(join(annotationsDir, `${sample}.txt`));
+        it('should parse and return an object', { timeout: 500 }, async () => {
+          data = await parseBlueprintData(stream, annotation);
 
           expect(data).toBeTruthy();
 
           // Verify data is serializable
           expect(() => JSON.stringify(data)).not.toThrow();
-
-          if (blueprintStrings[sample]) {
-            expect(checkBlueprintDataMatchesString(data.blueprints[0], blueprintStrings[sample])).toBe(true);
-          }
         });
+
+        if (blueprintStrings[sample]) {
+          it('should match blueprint string', () => {
+            expect(checkBlueprintDataMatchesString(data.blueprints[0], blueprintStrings[sample])).toBe(true);
+          });
+        }
+
+        const originalFilesize = (await stat(path)).size;
+
+        afterAll(() => annotation.finish(stream, originalFilesize), 400);
       });
     }
   });
