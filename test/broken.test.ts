fb1427128cde4d8f82d65c6a461a02b2ae6434cb
Cameron Tacklind <cameron@tacklind.com>
2025-01-27 01:49:16 -0800

    Regroup tests

---
+++ test/broken.test.ts
@@ -0,0 +1,17 @@
+import { it, expect, describe } from 'vitest';
+import { parseBlueprintData } from '../src/index.js';
+import { createReadStream } from 'node:fs';
+
+describe('Broken', { concurrent: true, timeout: 1000 }, () => {
+  it(`should handle errors for corrupted stream`, async () => {
+    // Create a corrupted stream by using a json file
+    const corruptedStream = createReadStream('package.json');
+    await expect(parseBlueprintData(corruptedStream)).rejects.toThrow();
+  });
+
+  it('should handle non-existent file', async () => {
+    const nonExistentStream = createReadStream('nonexistent.dat');
+    nonExistentStream.on('error', () => {});
+    await expect(parseBlueprintData(nonExistentStream)).rejects.toThrow();
+  });
+});
--- test/samples.test.ts
+++ test/samples.test.ts
@@ -8,60 +8,44 @@ import { join } from 'node:path';
 import annotationWriter from './helpers/annotationWriter.js';
 import { BlueprintData } from '../src/BlueprintData.js';
 
-const samplesDir = 'test/samples';
+const SamplesDir = 'test/samples';
 
-describe('Blueprint Parser', { concurrent: true, timeout: 1000 }, () => {
-  describe('Samples', async () => {
-    // We have the blueprint strings in a yaml file for some samples
-    const blueprintStrings = parse(await readFile(join(samplesDir, 'exports.yaml'), 'utf-8')) as Record<string, string>;
+describe('Samples', { concurrent: true, timeout: 1000 }, async () => {
+  // We have the blueprint strings in a yaml file for some samples
+  const blueprintStrings = parse(await readFile(join(SamplesDir, 'exports.yaml'), 'utf-8')) as Record<string, string>;
 
-    const annotationsDir = join(samplesDir, 'annotated');
+  const annotationsDir = join(SamplesDir, 'annotated');
 
-    await mkdir(annotationsDir, { recursive: true });
+  await mkdir(annotationsDir, { recursive: true });
 
-    const sampleFiles = (await readdir(samplesDir)).filter(file => file.endsWith('.dat'));
+  const sampleFiles = (await readdir(SamplesDir)).filter(file => file.endsWith('.dat'));
 
-    for (const sample of sampleFiles) {
-      describe(sample, async () => {
-        const path = join(samplesDir, sample);
+  for (const sample of sampleFiles) {
+    describe(sample, async () => {
+      const path = join(SamplesDir, sample);
 
-        // Capture the data and any potential error
-        let data: BlueprintData;
-        const stream = createReadStream(path);
-        const annotation = annotationWriter(join(annotationsDir, `${sample}.txt`));
-        it('should parse and return an object', { timeout: 500 }, async () => {
-          data = await parseBlueprintData(stream, annotation);
+      // Capture the data and any potential error
+      let data: BlueprintData;
+      const stream = createReadStream(path);
+      const annotation = annotationWriter(join(annotationsDir, `${sample}.txt`));
+      it('should parse and return an object', { timeout: 500 }, async () => {
+        data = await parseBlueprintData(stream, annotation);
 
-          expect(data).toBeTruthy();
+        expect(data).toBeTruthy();
 
-          // Verify data is serializable
-          expect(() => JSON.stringify(data)).not.toThrow();
+        // Verify data is serializable
+        expect(() => JSON.stringify(data)).not.toThrow();
 
-          if (blueprintStrings[sample]) {
-            it('should match blueprint string', () => {
-              expect(checkBlueprintDataMatchesString(data.blueprints[0], blueprintStrings[sample])).toBe(true);
-            });
-          }
-        });
-
-        const originalFilesize = (await stat(path)).size;
-
-        afterAll(() => annotation.finish(stream, originalFilesize), 400);
+        if (blueprintStrings[sample]) {
+          it('should match blueprint string', () => {
+            expect(checkBlueprintDataMatchesString(data.blueprints[0], blueprintStrings[sample])).toBe(true);
+          });
+        }
       });
-    }
-  });
 
-  return;
-  // Broken
+      const originalFilesize = (await stat(path)).size;
 
-  it(`should handle errors for corrupted stream`, async () => {
-    // Create a corrupted stream by using a json file
-    const corruptedStream = createReadStream(`package.json`);
-    await expect(parseBlueprintData(corruptedStream)).rejects.toThrow();
-  });
-
-  it('should handle non-existent file', async () => {
-    const nonExistentStream = createReadStream(`${samplesDir}/nonexistent.dat`);
-    await expect(parseBlueprintData(nonExistentStream)).rejects.toThrow();
-  });
+      afterAll(() => annotation.finish(stream, originalFilesize), 400);
+    });
+  }
 });
