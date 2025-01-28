791e597db0336d43803c2ebd4404661a64d8d5b2
Cameron Tacklind <cameron@tacklind.com>
2025-01-28 01:47:53 -0800

    Add folder structure to samples and move annotated files

--- test/samples.test.ts
+++ test/samples.test.ts
@@ -4,32 +4,30 @@ import { createReadStream } from 'node:fs';
 import { parse } from 'yaml';
 import { checkBlueprintDataMatchesString } from './helpers/compare.js';
 import { mkdir, readdir, readFile, stat } from 'node:fs/promises';
-import { join } from 'node:path';
+import { dirname, join } from 'node:path';
 import annotationWriter from './helpers/annotationWriter.js';
-import { BlueprintData } from '../src/BlueprintData.js';
 
-const SamplesDir = 'test/samples';
+const SamplesDir = join(import.meta.dirname, 'samples');
+
+// TODO: Allow env override?
+const annotationsDir = join(import.meta.dirname, 'annotated');
 
 describe('Samples', { concurrent: true, timeout: 1000 }, async () => {
   // We have the blueprint strings in a yaml file for some samples
-  const blueprintStrings = parse(await readFile(join(SamplesDir, 'exports.yaml'), 'utf-8')) as Record<string, string>;
-
-  const annotationsDir = join(SamplesDir, 'annotated');
-
-  await mkdir(annotationsDir, { recursive: true });
-
-  const sampleFiles = (await readdir(SamplesDir)).filter(file => file.endsWith('.dat'));
+  const { blueprintStrings, sampleFiles } = await loadSamples();
 
   for (const sample of sampleFiles) {
     describe(sample, async () => {
       const path = join(SamplesDir, sample);
-
-      // Capture the data and any potential error
-      let data: BlueprintData;
       const stream = createReadStream(path);
-      const annotation = annotationWriter(join(annotationsDir, `${sample}.txt`));
+      const outPath = join(annotationsDir, `${sample}.txt`);
+
+      await mkdir(dirname(outPath), { recursive: true });
+
+      const annotation = annotationWriter(outPath);
+
       it('should parse and return an object', { timeout: 500 }, async () => {
-        data = await parseBlueprintData(stream, annotation);
+        const data = await parseBlueprintData(stream, annotation);
 
         expect(data).toBeTruthy();
 
@@ -49,3 +47,22 @@ describe('Samples', { concurrent: true, timeout: 1000 }, async () => {
     });
   }
 });
+
+async function loadSamples() {
+  const sampleFiles = (await readdir(SamplesDir, { recursive: true })).filter(file => file.endsWith('.dat'));
+  const exportsFiles = (await readdir(SamplesDir, { recursive: true })).filter(file =>
+    file.match('(^|[/\\\\])exports.yaml$'),
+  );
+
+  const blueprintStrings: Record<string, string> = {};
+
+  for (const exportsFile of exportsFiles) {
+    const dir = dirname(exportsFile);
+    const exports = parse(await readFile(join(SamplesDir, exportsFile), 'utf-8')) as Record<string, string>;
+    for (const [key, value] of Object.entries(exports)) {
+      blueprintStrings[join(dir, key)] = value;
+    }
+  }
+
+  return { blueprintStrings, sampleFiles, exportsFiles };
+}
