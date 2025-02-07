fb890b2ee100ef8bc1e4825ac2d40150762a161b
Cameron Tacklind <cameron@tacklind.com>
2025-02-07 03:06:31 -0800

    Enable env override

--- src/util/getBlueprintLocation.ts
+++ src/util/getBlueprintLocation.ts
@@ -1,5 +1,15 @@
-// TODO: Make this work on Linux and Mac
-const factorioDir = process.env.APPDATA + '/Factorio';
+import { join } from 'node:path';
+
+function getUserDir() {
+  if (process.platform === 'win32') {
+    return process.env.APPDATA;
+  }
+
+  // TODO: Test this on Linux and Mac
+  return process.env.HOME;
+}
+
+const factorioDir = process.env.FACTORIO_DIR ?? join(getUserDir() ?? '.', 'Factorio');
 
 export function getBlueprintLocation(v2 = true) {
   return `${factorioDir}/blueprint-storage${v2 ? '-2' : ''}.dat`;
--- test/helpers/dirs.ts
+++ test/helpers/dirs.ts
@@ -3,7 +3,6 @@ import { join } from 'path/posix';
 // TODO: cwd?
 export const WorkDir = '.';
 
-// TODO: Allow env override?
-export const AnnotationsDir = join(WorkDir, 'test', 'samples-annotated');
+export const AnnotationsDir = process.env.ANNOTATIONS_DIR ?? join(WorkDir, 'test', 'samples-annotated');
 
-export const SamplesDir = join(WorkDir, 'test', 'samples');
+export const SamplesDir = process.env.SAMPLES_DIR ?? join(WorkDir, 'test', 'samples');
